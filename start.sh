c0242692fa59760693b9fd58772c88b6bc5819f6
Krakaw <41575888+Krakaw@users.noreply.github.com>
2020-10-22 12:31:31 +0200

    Added cron to schedule docker login

--- start.sh
+++ start.sh
@@ -4,5 +4,8 @@ export DOCKER_BIN="${DOCKER_BIN:-$(which docker)}"
 export CUID="${UID:-$(id -u)}"
 export CGID="${GID:-$(id -g)}"
 docker network create controller-network || echo "Network already exists"
+if [[ -n "$REBUILD" ]]; then
+  docker-compose build --no-cache controller
+fi
 docker-compose up -d
-docker-compose exec controller sh -c "cd /controller/controller && ./start-controller.sh"
+docker-compose exec controller sh -c "cron && cd /controller/controller && ./start-controller.sh"
