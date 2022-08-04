18a3e35070b8efb4d9df945b448bdf413c7e11ff
Krakaw <41575888+Krakaw@users.noreply.github.com>
2022-08-04 17:00:26 +0200

    Add ability to add volumes to root docker images

--- controller/build-docker-compose.sh
+++ controller/build-docker-compose.sh
@@ -3,6 +3,7 @@
 TEMPLATE_FILE=$1
 OUTPUT_FILE=$2
 SERVICES_DIR="${3:-./docker-services.d/}"
+VOLUMES_DIR="${4:-./docker-services.d/volumes/}"
 
 TEMPLATE_STRINGS=""
 for TEMPLATE in "${SERVICES_DIR}"*.yml; do
@@ -11,3 +12,14 @@ for TEMPLATE in "${SERVICES_DIR}"*.yml; do
 done
 
 sed "s!{TEMPLATES}!${TEMPLATE_STRINGS//&/\\&}!" "$TEMPLATE_FILE" | tr '§' '\n' >"$OUTPUT_FILE"
+
+if [[ -d "$VOLUMES_DIR" ]]; then
+  VOLUMES_STRINGS=""
+  for VOLUME in "${VOLUMES_DIR}"*.yml; do
+    VOLUMES_STRING=$(tr <"$VOLUME" '\n' '§')
+    VOLUMES_STRINGS="$VOLUMES_STRINGS§$VOLUMES_STRING"
+  done
+
+  sed "s!{VOLUMES}!${VOLUMES_STRING//&/\\&}!" "$OUTPUT_FILE" | tr '§' '\n' >"$OUTPUT_FILE.tmp"
+  mv "$OUTPUT_FILE.tmp" "$OUTPUT_FILE"
+fi
